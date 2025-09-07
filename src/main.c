15d67db4a4bd4d63b65769cd087117bb516320af
Ahmed <braika7med@gmail.com>
2025-09-07 20:56:46 +0100

    feat(cli): add help message and support passing multiple expressions as arguments

--- src/main.c
+++ src/main.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "eval.h"
 #include "repl.h"
 
-int main(int arg, char const *argv[]) {
-    if (arg > 2) {
-        fprintf(stderr, "Usage: %s [expression]", argv[0]);
-        return -1;
+int main(int argc, char const *argv[]) {
+    if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
+        printf("Usage: %s [expressions...]\n", argv[0]);
+        printf("  -h, --help                Show this help message\n");
+        printf("\nExamples:\n");
+        printf("  %s                        Start interactive REPL\n", argv[0]);
+        printf("  %s x=2pi 'sin(x*ln 2)'    Evaluate expressions and print results\n", argv[0]);
+        return 0;
     }
 
     EvalContext ctx;
     eval_init(&ctx);
 
-    if (arg == 2) {
-        Number result = eval(&ctx, argv[1]);
-        if (ctx.error_type) {
-            fprintf(stderr, "Error: %s\n", ctx.error_msg);
-            return -1;
-        }
-        printf("%s\n", eval_stringify(NULL, 0, result));
-    } else
+    if (argc == 1)
         repl(&ctx);
+    else
+        for (int i = 1; i < argc; i++) {
+            Number result = eval(&ctx, argv[i]);
+            if (ctx.error_type) {
+                fprintf(stderr, "Error: %s\n", ctx.error_msg);
+                return -1;
+            }
+            puts(eval_stringify(NULL, 0, result));
+        }
 
     eval_end(&ctx);
     return 0;
